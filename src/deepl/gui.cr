c73b8bec77535efb26cf4a3048f89d7acb3bca70
kojix2 <2xijok@gmail.com>
2024-10-30 17:47:44 +0900

    DSL style

--- src/deepl/gui.cr
+++ src/deepl/gui.cr
@@ -38,10 +38,11 @@ def perform_translation(source_lang_dropdown, target_lang_dropdown, source_text_
 end
 
 app.activate_signal.connect do
-  Gtk::ApplicationWindow.new(app).tap do |window|
+  Gtk::ApplicationWindow.new(app).tap { |window|
     window.title = "DeepL Translator"
     window.set_default_size(800, 400)
-    window.child = Gtk::Box.new(:vertical, 10).tap do |main_box|
+
+    window.child = Gtk::Box.new(:vertical, 10).tap { |main_box|
       main_box.margin_top = 10
       main_box.margin_bottom = 10
       main_box.margin_start = 10
@@ -58,93 +59,75 @@ app.activate_signal.connect do
           css_provider.load_from_string(css)
 
           translate_button = Gtk::Button.new_with_label("Translate")
-          source_lang_dropdown = Gtk::DropDown.new_from_strings(
-            SOURCE_LANGUAGES.map(&.name).unshift("AUTO")
-          )
+          source_lang_dropdown = Gtk::DropDown.new_from_strings(SOURCE_LANGUAGES.map(&.name).unshift("AUTO"))
           source_text_view = Gtk::TextView.new
 
-          text_box.append(
-            Gtk::Box.new(:vertical, 10).tap do |lp|
-              lp.hexpand = true
-              lp.vexpand = true
+          text_box.append Gtk::Box.new(:vertical, 10).tap { |left_box|
+            left_box.hexpand = true
+            left_box.vexpand = true
 
-              lp.append(
-                source_lang_dropdown.tap do |l|
-                  l.selected = 0
-                  l.hexpand = false
-                  l.halign = :start
-                  l.set_size_request(180, -1)
-                end
-              )
+            left_box.append source_lang_dropdown.tap { |l|
+              l.selected = 0
+              l.hexpand = false
+              l.halign = :start
+              l.set_size_request(180, -1)
+            }
 
-              lp.append(
-                Gtk::ScrolledWindow.new.tap do |s|
-                  s.hexpand = true
-                  s.vexpand = true
-                  s.child = source_text_view.tap do |t|
-                    t.wrap_mode = :word
-                    t.style_context
-                      .add_provider(css_provider, Gtk::STYLE_PROVIDER_PRIORITY_USER.to_u32)
-                  end
-                end
-              )
+            left_box.append Gtk::ScrolledWindow.new.tap { |s|
+              s.hexpand = true
+              s.vexpand = true
+              s.child = source_text_view.tap { |t|
+                t.wrap_mode = :word
+                t.style_context
+                  .add_provider(css_provider, Gtk::STYLE_PROVIDER_PRIORITY_USER.to_u32)
+              }
+            }
 
-              lp.append(translate_button)
-            end
-          )
+            left_box.append(translate_button)
+          }
 
-          target_lang_dropdown = Gtk::DropDown.new_from_strings(
-            TARGET_LANGUAGES.map(&.name)
-          )
+          target_lang_dropdown = Gtk::DropDown.new_from_strings(TARGET_LANGUAGES.map(&.name))
           target_text_view = Gtk::TextView.new
 
-          text_box.append(
-            Gtk::Box.new(:vertical, 10).tap do |rp|
-              rp.hexpand = true
-              rp.vexpand = true
+          text_box.append Gtk::Box.new(:vertical, 10).tap { |rp|
+            rp.hexpand = true
+            rp.vexpand = true
 
-              rp.append(
-                target_lang_dropdown.tap do |l|
-                  l.selected = default_target_lang_index
-                  l.hexpand = false
-                  l.halign = :start
-                  l.set_size_request(180, -1)
-                end
-              )
+            rp.append target_lang_dropdown.tap { |l|
+              l.selected = default_target_lang_index
+              l.hexpand = false
+              l.halign = :start
+              l.set_size_request(180, -1)
+            }
 
-              rp.append(
-                Gtk::ScrolledWindow.new.tap do |s|
-                  s.hexpand = true
-                  s.vexpand = true
-                  s.child = target_text_view.tap do |t|
-                    t.wrap_mode = :word
-                    t.editable = false
-                    t.style_context
-                      .add_provider(css_provider, Gtk::STYLE_PROVIDER_PRIORITY_USER.to_u32)
-                  end
-                end
-              )
+            rp.append Gtk::ScrolledWindow.new.tap { |s|
+              s.hexpand = true
+              s.vexpand = true
+              s.child = target_text_view.tap { |t|
+                t.wrap_mode = :word
+                t.editable = false
+                t.style_context
+                  .add_provider(css_provider, Gtk::STYLE_PROVIDER_PRIORITY_USER.to_u32)
+              }
+            }
 
-              rp.append(
-                Gtk::Button.new_with_label("Copy").tap do |copy_button|
-                  copy_button.clicked_signal.connect do
-                    target_text = target_text_view.buffer.text
-                    EasyClip.copy(target_text)
-                  end
-                end
-              )
-            end
-          )
+            rp.append Gtk::Button.new_with_label("Copy").tap { |copy_button|
+              copy_button.clicked_signal.connect do
+                target_text = target_text_view.buffer.text
+                EasyClip.copy(target_text)
+              end
+            }
+          }
 
           translate_button.clicked_signal.connect do
             perform_translation(source_lang_dropdown, target_lang_dropdown, source_text_view, target_text_view)
           end
         end
       )
-    end
+    }
 
     window.present
-  end
+  }
 end
 
 # Run the application
